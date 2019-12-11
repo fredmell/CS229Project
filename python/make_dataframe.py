05d413b04d6ebf1f175da8b17f1d7c80104516fe
Frederik Mellbye <frederme@stanford.edu>
2019-12-10 16:23:23 -0800

    Finalize make_dataframe and preprocess.py

--- python/make_dataframe.py
+++ python/make_dataframe.py
@@ -4,15 +4,32 @@ from loaddata import DataLoader
 from preprocess import Preprocess
 
 def main():
+    # Load and merge review, business and user data
     loader = DataLoader(path="../data/")
     df = loader.merge()
 
     preprocessor = Preprocess(df)
+
+    # Remove weekday columns
     preprocessor.rm_weekdays()
+
+    # Convert categorical variables to one-hot encoded variables and convert
+    # strings to numerical types if possible
     preprocessor.categorical_to_numerical()
+
+    # Fill NA observations with the mode of the given feature
     preprocessor.fill_na()
+
+    # Unravel and one-hot encode business categories
     preprocessor.unravel_categories()
+
+    # Sort observations by date and reindex with this ordering
     preprocessor.sort_by_date()
+
+    # Form temporal train-val-test split
+    preprocessor.split_data()
+
+    # Write the final dataframe to a pickle file
     preprocessor.dump("../data/yelp_df.pkl")
 
 if __name__ == "__main__":
--- python/preprocess.py
+++ python/preprocess.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import time
 import utils
 
 from tqdm import tqdm
@@ -8,7 +7,9 @@ from loaddata import DataLoader
 from cat_to_num import *
 
 class Preprocess:
-    """ Preprocesses data and performs data split.
+    """ Preprocesses data and performs data split. See individual method
+    docstrings for details. Each method modifies the dataframe attribute
+    in-place.
 
     """
     def __init__(self, df):
@@ -27,12 +28,12 @@ class Preprocess:
         num_predictors = len(self.df.columns)
 
         self.df.drop(labels=["Monday",
-                        "Tuesday",
-                        "Wednesday",
-                        "Thursday",
-                        "Friday",
-                        "Saturday",
-                        "Sunday"],
+                             "Tuesday",
+                             "Wednesday",
+                             "Thursday",
+                             "Friday",
+                             "Saturday",
+                             "Sunday"],
                      axis=1,
                      inplace=True)
 
@@ -59,7 +60,7 @@ class Preprocess:
         self.df['date'] = pd.to_datetime(self.df['date'])
 
     def fill_na(self):
-        """ Fill na values with mode of column in-place
+        """ Fill na values with mode of column in-place.
 
         """
         # Fill missing values with mode
@@ -74,20 +75,28 @@ class Preprocess:
             if column not in ignored_columns:
                 try:
                     mode = self.df[column].mode().iloc[0]
+                # Used for testing on subsets of data, should not occur
                 except IndexError:
                     print("Warning: No mode, imputed with NaN.")
                     mode = np.nan
+
                 self.df[column].fillna(value=mode, inplace=True)
                 print('{:03d} {:35s}'.format(j, column), mode)
 
     def count_categories(self):
+        """ Helper function to count the number of appearences of categories.
+
+        Returns:
+            sorted_vals (list) : Each entry a (category, count) pair, sorted by
+                                 counts.
+
+        """
         business_categories = self.df['categories']
 
         b_category_counts = {}
 
         # Iterate over reviews
         for row in range(business_categories.shape[0]):
-
             # Skip nones
             if business_categories[row] is None:
                 continue
@@ -114,7 +123,8 @@ class Preprocess:
 
     def unravel_categories(self, obs_thresh=100000):
         """ Unravel categories with number of observations greater than
-        obs_thresh and return the new data frame.
+        obs_thresh in-place. Adds a new column for each of these categories,
+        and one-hot encodes the precense of a category for a given business.
 
         Args:
             df (pd.DataFrame) : Dataset with column 'categories' to be unraveled
@@ -138,7 +148,34 @@ class Preprocess:
 
     def sort_by_date(self):
         self.df.sort_values(by='date', axis=0, inplace=True)
-        self.df.reset_index(drop=True)
+        self.df.reset_index(drop=True, inplace=True)
+
+    def split_data(self, splits=[0.8230, 0.9115]):
+        """ Split data into a train, validation and test set. The splits
+        indicate what quantiles to split at, the default is a 2004-2017
+        train set, a half 2018 validation set and the remaining half of 2018
+        reviews for the test set. Modifies the data in-place. A column
+        dataset is added indicating which dataset an observation belongs to.
+
+        Args:
+            splits (iterable) : Length 2 iterable with quantiles at which to
+                                split the data.
+
+        """
+        quantiles = df.date.quantile(splits, interpolation='nearest')
+
+        self.df['dataset'] = "train"
+
+        self.df['dataset'].where(self.df.date < quantiles[splits[0]],
+                                 other="val",
+                                 inplace=True)
+
+        self.df['dataset'].where(self.df.date < quantiles[splits[1]],
+                                 other="test",
+                                 inplace=True)
 
     def dump(self, filename="../data/yelp_df.pkl"):
+        """ Dump current data frame to file named filename.
+
+        """
         self.df.to_pickle(filename)
